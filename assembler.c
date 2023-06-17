57cb43c14759621cf1205ec502919a0a09abea73
Naty55 <64107292+naty55@users.noreply.github.com>
2023-06-17 21:13:27 +0300

    use preassembler

--- assembler.c
+++ assembler.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #include "assembler.h"
+#include "pre_assembler.h"
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int main(int argc, char const *argv[]) {
+    FILE * i_file = fopen("test.as", "r");
+    FILE * s_file = fopen("test.am", "w");
+    remove_macros(i_file, s_file);
+    fclose(i_file);
+    fclose(s_file);
     return 0;
 }
