d04e02579cf91b021149b579c8824d9682f72f94
Naty55 <64107292+naty55@users.noreply.github.com>
2023-06-17 21:13:10 +0300

    Added constants

--- constants.h
+++ constants.h
@@ -1,6 +1,14 @@
+#ifndef __CONST_H
+#define __CONST_H
+#define MAX_LINE_SIZE 100
+
+#define MACRO "mcro"
+#define ENDMACRO "endmcro"
+
 enum encoding {
     A = 0,
     E = 1,
     R = 2
 };
 
+#endif
