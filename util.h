05670083b78a36f7d5affdf98df79952aca16139
Naty55 <64107292+naty55@users.noreply.github.com>
2023-06-17 21:28:14 +0300

    Add header check

--- util.h
+++ util.h
@@ -1,4 +1,5 @@
-
+#ifndef __UTIL_H
+#define __UTIL_H
 typedef enum {
     False = 0,
     True = 1
@@ -6,3 +7,5 @@ typedef enum {
 
 char * skip_spaces(char * str);
 Bool is_str_empty(char * str);
+
+#endif
