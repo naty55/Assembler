efaabddc143ba9c77a664022afcb0b2178cbff69
Naty55 <64107292+naty55@users.noreply.github.com>
2023-06-17 21:12:01 +0300

    Added functionality to list

--- list.c
+++ list.c
@@ -1,5 +1,7 @@
 #include "list.h"
 #include <stdlib.h>
+#include <string.h>
+#include "util.h"
 
 struct CharList {
     char *data;
@@ -9,8 +11,8 @@ struct CharList {
 
 char_list create_char_list() {
     char_list list = malloc(sizeof(struct CharList));
-    list->data=NULL;
-    list->size = 0; 
+    list->data= malloc(1);
+    list->size = 1; 
     list->length = 0;
     return list;
 }
@@ -24,13 +26,54 @@ char append_char(char_list l, char c) {
     l->data[l->length++] = c;
     return c;
 }
-char append_all_chars(char_list l, char *items, int size) {
+char append_n_chars(char_list l, char *items, int size) {
     int i;
     for (i = 0; i < size; i++) {
         append_char(l, items[i]);
     }
     return items[i];
 }
+
+char append_chars(char_list l, char *items) {
+    int i;
+    for (i = 0; items[i] != 0; i++) {
+        append_char(l, items[i]);
+    }
+    return items[i];
+}
+
+char read_string(char_list l, char * str) {
+    str = skip_spaces(str);
+    while(!isspace(*str) && *str != '\n' && *str != '\0') {
+        append_char(l, *str);
+        str++;
+    }
+    return *(str - 1);
+}
+
 char get_char_from_list(char_list l, int i) {
     return (l->data)[i];
+}
+
+unsigned long get_length(char_list l) {
+    return l->length;
+}
+
+char * list_to_string(char_list l) {
+    char * str = malloc((l->length + 1) * sizeof(char));
+    strncpy(str, l->data, l->length);
+    str[l->length] = '\0';
+    return str;
+}
+
+char_list clear_list(char_list l) {
+    l->length = 0;
+}
+Bool is_empty(char_list l) {
+    return (Bool)(l->length == 0);
+}
+
+void free_list(char_list l) {
+    free(l->data);
+    free(l);
 }
--- list.h
+++ list.h
@@ -1,10 +1,17 @@
 #ifndef __LIST_H
 #define __LIST_H
+#include "util.h"
 typedef struct CharList *char_list;
 
 char_list create_char_list();
 char append_char(char_list l, char item);
-char append_all_chars(char_list l, char *items, int size);
-char get_char(char_list l, int i);
-
+char append_chars(char_list l, char *items);
+char append_n_chars(char_list l, char *items, int size);
+char read_string(char_list l, char * str);
+char get_char_from_list(char_list l, int i);
+unsigned long get_length(char_list l);
+char * list_to_string(char_list l);
+char_list clear_list(char_list l);
+Bool is_empty(char_list l);
+void free_list(char_list l);
 #endif
