462edddc3157baa19d17870fc8a49bd6f2298dd8
Naty55 <64107292+naty55@users.noreply.github.com>
2023-06-25 22:44:03 +0300

    main changes

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 int main(int argc, char const *argv[]) {
     char file_as[] = "test.as";
     char file_am[] = "test.am";
-    FILE * i_file = fopen("test.as", "r");
-    FILE * s_file = fopen("test.am", "w");
+    FILE * i_file = fopen(file_as, "r");
+    FILE * s_file = fopen(file_am, "w");
     remove_macros(i_file, s_file);
     fclose(i_file);
     fclose(s_file);
