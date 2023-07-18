ad9d5f63b3bcecfbe8294b0e3d4968abe2545648
Naty55 <64107292+naty55@users.noreply.github.com>
2023-07-18 23:31:31 +0300

    cosmetics

--- pre_assembler.c
+++ pre_assembler.c
@@ -12,7 +12,7 @@ int remove_macros(FILE *input_file, FILE* source_file) {
     char *ptr_in_line;
     clist macro_name = create_clist();
     clist macro_content;
-    clist_table macrosTable = create_clist_table();
+    ptable macrosTable = create_ptable();
     while(fgets(line, MAX_LINE_SIZE, input_file) != NULL) {
         /*printf("%d\t: %s", i++, line);*/
         if(!is_clist_empty(macro_name)) {
@@ -26,14 +26,14 @@ int remove_macros(FILE *input_file, FILE* source_file) {
         } else {
             ptr_in_line =  skip_spaces(line);
             if(strncmp(MACRO, ptr_in_line, 4) == 0) {
-                read_string(macro_name, ptr_in_line + 4);
+                clist_read_string(macro_name, ptr_in_line + 4);
                 macro_content = create_clist();
-                add_to_clist_table(macrosTable, list_to_string(macro_name), macro_content);
+                ptable_insert(macrosTable, list_to_string(macro_name), macro_content);
             } else {
                 char *macro_name_array;
-                read_string(macro_name, ptr_in_line);
+                clist_read_string(macro_name, ptr_in_line);
                 macro_name_array = list_to_string(macro_name);
-                macro_content = get_from_clist_table(macrosTable, macro_name_array);
+                macro_content = ptable_get(macrosTable, macro_name_array);
                 if(macro_content != NULL) {
                     char *content_array = list_to_string(macro_content);
                     fputs(content_array, source_file);
@@ -47,7 +47,7 @@ int remove_macros(FILE *input_file, FILE* source_file) {
         }
     }
     /*printf("\n");*/
-    free_clist_table(macrosTable);
+    free_ptable(macrosTable);
     free_clist(macro_name);
     return 0;
 }
--- symbol.h
+++ symbol.h
@@ -1,8 +1,12 @@
+#ifndef __SYMBOL_H
+#define __SYMBOL_H
 #include "constants.h"
 typedef struct Symbol *symbol; 
 
-
+symbol create_symbol();
 int symbol_get_offset(symbol sym);
 encoding symbol_get_encoding(symbol sym);
 void symbol_set_offset(symbol sym, int offset);
 void symbol_set_encoding(symbol sym, encoding encode);
+
+#endif
